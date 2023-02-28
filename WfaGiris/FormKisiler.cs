ff6ea23d59acd7e28ca441e7a37edc5fbdfcd218
Erdener EROGLU <erdener.eroglu98@hotmail.com>
2023-02-28 10:44:12 +0300

    .

--- WfaGiris/FormKisiler.cs
+++ WfaGiris/FormKisiler.cs
@@ -42,7 +42,26 @@ public partial class FormKisiler : Form
         {
             if(item is TextBox)
                 item.Text = String.Empty;
+            else if(item is DateTimePicker dPicker)
+            {
+                dPicker.Value = DateTime.Now;
+                //(item as DateTimePicker).Value = DateTime.Now;
+                //((DateTimePicker)item).Value = DateTime.Now;
+                //farkını araştır.
+            }
+            else if(item is CheckBox cBox)
+            {
+                cBox.Checked = false;
+            }
+            else if(item is ComboBox comboBox)
+            {
+                comboBox.SelectedIndex = -1;
+            }
+            else if (item is ListBox lstBox)
+            {
+                lstBox.SelectedIndex = -1;
+            }
         }
-        dtpDogumTarihi.Value = DateTime.Now;
+        
     }
 }
