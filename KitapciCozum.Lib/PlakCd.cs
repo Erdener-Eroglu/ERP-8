131c066106289faaee010d94b32cdc6768a4cdb9
Erdener EROGLU <erdener.eroglu98@hotmail.com>
2023-03-03 13:43:50 +0300

    .

---
+++ KitapciCozum.Lib/PlakCd.cs
@@ -0,0 +1,10 @@
+﻿namespace KitapciCozum.Lib;
+
+public class PlakCd : Urun
+{
+    public Artist Artist { get; set; } //Complex Type
+    public string AlbumAdi { get; set; }
+    public string Tur { get; set; }
+}
+
+
