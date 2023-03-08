c7ba93288f042be1d10a7e2742ebe474e8a440d7
Erdener EROGLU <erdener.eroglu98@hotmail.com>
2023-03-08 09:45:24 +0300

    .

---
+++ AracEnvanter/Form1.cs
@@ -0,0 +1,10 @@
+namespace AracEnvanter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AracEnvanter/Models/Arac.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AracEnvanter.Models;
+public abstract class BaseModel
+{
+    public Guid Id { get; set; } = new Guid();
+    public DateTime CreatedTime { get; set; } = DateTime.Now;
+}
+public class Arac : BaseModel
+{
+    public Model Model { get; set; }
+    public string Plaka { get; set; }
+}
+public class Marka : BaseModel
+{
+    public string Ad { get; set; }
+
+    public byte[] Logo { get; set; }
+    public List<Model> Modeller { get; set; } = new List<Model>();
+
+}
+public class Model : BaseModel
+{
+    public string Ad { get; set; }
+    public KasaTipi KasaTipi { get; set; }
+    public Marka Marka { get; set; }
+    public List<Arac> Araclar { get; set; } = new List<Arac>();
+}
+
+public enum KasaTipi
+{
+    HatchBack,
+    Sedan,
+    StationWagon,
+    Coupe,
+    Convertible,
+    SUV,
+    Crossover,
+    Van,
+    Pickup,
+    Minivan,
+    Minibus,
+    Limousine,
+    Roadster,
+    Targa
+}
