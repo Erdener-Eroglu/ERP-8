83a813f54fd3fc287f11f4150b418bf43fb58a1b
Erdener EROGLU <erdener.eroglu98@hotmail.com>
2023-03-16 09:24:33 +0300

    .

--- InterfaceOrnek_Odeme/Entities/KrediKartiylaOdeme.cs
+++ InterfaceOrnek_Odeme/Entities/KrediKartiylaOdeme.cs
@@ -10,19 +10,91 @@ namespace InterfaceOrnek_Odeme.Entities
 {
     internal class KrediKartiylaOdeme : Odeme, IOdemeAlabilir
     {
-        public string KartSahibi { get; set; }
-        public string KartNumarasi { get; set; }
+        private string _kartSahibi;
+        public string KartSahibi
+        {
+            get
+            {
+                return _kartSahibi;
+            }
+            set
+            {
+                bool hepsiHarfMi = true;
+                if(value.Length < 5)
+                {
+                    throw new Exception("Lütfen bilgilerinizi geçerli giriniz");
+                }
+                foreach(var item in value) 
+                {
+                    if(!char.IsWhiteSpace(item) && !char.IsLetter(item))
+                    {
+                        hepsiHarfMi = false; 
+                        break;
+                    }
+                }
+                if(!hepsiHarfMi) 
+                {
+                    throw new Exception("Geçerli bilgi giriniz");
+                }
+                else
+                {
+                    _kartSahibi = value;
+                }
+
+            }
+        }
+        private string _kartNumarasi;
+        public string KartNumarasi
+        {
+            get
+            {
+                return _kartNumarasi;
+            }
+            set
+            {
+                bool kuraliBozanVarMi = false;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (i == 4 || i == 9 || i == 14)
+                    {
+                        if (!char.IsWhiteSpace(value[i]))
+                        {
+                            kuraliBozanVarMi = true;
+                            break;
+                        }
+                    }
+                    if (!char.IsDigit(value[i]))
+                    {
+                        kuraliBozanVarMi = true;
+                        break;
+                    }
+                }
+
+                if (value.Length != 19)
+                {
+                    throw new Exception("Kart numarası 16 haneli olmak zorundadır");
+                }
+                else if (kuraliBozanVarMi)
+                {
+                    throw new Exception("Kart numarası sadece rakamlardan oluşmalıdır!");
+                }
+                else
+                {
+                    _kartNumarasi = value;
+                }
+            }
+        }
         public byte SonKullanimAy { get; set; }
         private byte _sonKullanimYil;
-        public byte SonKullnaimYil 
-        { 
+        public byte SonKullnaimYil
+        {
             get
             {
                 return _sonKullanimYil;
             }
             set
             {
-                if(value > 22)
+                if (value > 22)
                 {
                     _sonKullanimYil = value;
                 }
